83d9247d6a093bf7d9eb4b8f7898af0011c0ef8b
IlliaShulha <102927025+IlliaShulha@users.noreply.github.com>
2023-03-16 21:09:35 -0400

    initial

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+const tasksData = fs.readFileSync('tasksData.json');
+const tasks = JSON.parse(tasksData);
+
+// Allow cross-origin requests from any domain
+app.use(cors(origin = "*"));
+
+// Define your routes and middleware here
+// ...
+
+const port = process.env.PORT || 3000;
+
+app.get('/tasks', (req, res) => {
+    res.send(tasks);
+});
+
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
